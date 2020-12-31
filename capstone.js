72e9d6abb92459715e8aacdabce3312fee9b2619
shusweela <shusweela@gmailcom>
2020-12-31 01:12:35 -0600

    navigation added

--- capstone.js
+++ capstone.js
@@ -1,12 +1,12 @@
 
-
+//sends confirmation to customer that we recieved the request and emailed them for further steps
 function submitDisplay() {
     document.getElementById("submitButton").innerHTML = 
     "Thank you for your request! An email has been sent with directions for the next step.";
 }
 
-
+//button on admin page that submits validation password to be confirmed to allow access to admins database
 function adminGo() {
     document.getElementById("goButton").innerHTML = 
-    
+     "Thank you....";
 }
